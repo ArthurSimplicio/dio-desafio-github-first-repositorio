f50a465426465850e1fcd735d77b9b0d905c224e
ArthurSimplicio <arthurpark20@gmail.com>
2023-09-25 17:21:52 -0300

    Desafio concluido

---
+++ Desafio-felipao/index.js
@@ -0,0 +1,25 @@
+let nome = ''; let xp = 0; let nivel = '';
+
+if (xp < 1000) {
+    nivel = 'Ferro'
+    console.log();
+} else if (xp >= 1000 && xp < 2000){
+    nivel = 'Broze'
+} else if (xp >= 2000 && xp < 6000){
+    nivel = 'Prata' 
+} else if (xp >= 6000 && xp < 7000){
+    nivel = 'Ouro'
+} else if (xp >= 7000 && xp < 8000){
+    nivel = 'Platina'
+} else if (xp >= 8000 && xp < 9000){
+    nivel = 'Ascendente'
+} else if (xp >= 9000 && xp < 10000){
+    nivel = 'Imortal'
+} else if (xp >= 10000){
+    nivel = 'Radiante'
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+
+
+
